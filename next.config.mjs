b75d0dd3e1c85a2d5e6472eaf11786d83a7d12be
vasiliykozlov <v.kozlov@dats.team>
2024-03-24 19:11:13 +0300

    add header

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
-import path from 'path';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack(config, { webpack }) {
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            react: path.resolve('./node_modules/react'),
-            'react-dom': path.resolve('./node_modules/react-dom'),
-            'react-redux': path.resolve('./node_modules/react-redux'),
-            'next-i18next': path.resolve('./node_modules/next-i18next'),
-            next: path.resolve('./node_modules/next'),
-            'react-datepicker': path.resolve('./node_modules/react-datepicker'),
-        };
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,5 +1,14 @@
 import type { FC } from 'react';
 
-export const Header: FC = () => {
-  return <div />;
+import type { HeaderProps } from './Header.types';
+
+import styles from './Header.module.scss';
+
+export const Header: FC<HeaderProps> = ({ vacancy }) => {
+  return (
+    <header className={styles.root}>
+      <div className={styles.side}>HR-test</div>
+      <div className={styles.main}>{vacancy}</div>
+    </header>
+  );
 };
